beab86dd473f6ee151091dfdd00f430e433277a1
Test <101502649+susamobus@users.noreply.github.com>
2022-05-22 20:42:56 +0800

    sus now

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-
+var amogus = 0
+var sus = 0
 function amogus() {
-    var amogus = "test"
-    document.getElementById("sus").innerHTML = amogus
+    var amogus = amogus+1
 };
+setInterval(function(){
+    document.getElementById("sus").innerHTML = sus
+    document.getElementById("amogus").innerHTML = amogus
+    var sus = sus+1*amogus
+},50)
