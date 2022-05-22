bfe6ecdb5c55266774aff85534050a00c74885d6
Test <101502649+susamobus@users.noreply.github.com>
2022-05-22 20:47:05 +0800

    sus

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var amogus = 0
 var sus = 0
 function amogus() {
-    var amogus = amogus+1
+    amogus = amogus+1
 };
 setInterval(function(){
     document.getElementById("sus").innerHTML = sus
     document.getElementById("amogus").innerHTML = amogus
-},50)
+},1)
 setInterval(function(){
     sus = sus+(1*amogus)
-},500)
+},5)
