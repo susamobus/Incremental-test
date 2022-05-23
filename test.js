a1becf2518c162921a0b401971d5483b62dcd64e
Test <101502649+susamobus@users.noreply.github.com>
2022-05-23 20:04:56 +0800

    work now plsssdssssdfgbdrtbftyhrt

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
-var number = 0
+var sus = 0
+var amogus = 0
 window.setInterval(function() {
-    number = number + 1
+    sus = sus + 1 * amogus
 }, 500);
+window.setInterval(function() {
+    document.getElementById("sus").innerHTML = sus
+    document.getElementById("amogus").innerHTML = amogus
+},50)
 function Amogus() {  
-    document.getElementById("Sus").innerHTML = number
+    amogus = amogus + 1
 };
 //setInterval(function(){
 //    document.getElementById("sus").innerHTML = sus
