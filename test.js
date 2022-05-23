61a2a6e9f8afe224992ef1755d9412a03cfee42f
MCi-007 <82216395+MCi-007@users.noreply.github.com>
2022-05-23 19:52:07 +0700

    a

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var sus = 0
 var amogus = 0
 window.setInterval(function() {
-    sus = sus + 1 * amogus
-}, 500);
+    sus = sus + 0.1 * amogus
+}, 35);
 window.setInterval(function() {
     document.getElementById("sus").innerHTML = sus
     document.getElementById("amogus").innerHTML = amogus
