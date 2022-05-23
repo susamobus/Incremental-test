7f304728f6796429cb6d5fc6ec9bb7f7fb2765b8
Test <101502649+susamobus@users.noreply.github.com>
2022-05-23 20:52:58 +0800

    ok

--- test.js
+++ test.js
@@ -1,12 +1,23 @@
 var sus = 0
-var amogus = 0
+var amogus = 0;
 window.setInterval(function() {
     sus = sus + 0.1 * amogus
 }, 35);
 window.setInterval(function() {
     document.getElementById("sus").innerHTML = sus
     document.getElementById("amogus").innerHTML = amogus
-},50)
+    Save()
+},50);
 function Amogus() {  
     amogus = amogus + 1
+};
+function Save() {
+    var save = {
+        Sus: sus,
+        Amogus: amogus
+    }
+    localStorage.setItem("save",JSON.stringify(save));
+}
+function Load() {
+    var load = JSON.parse(localStorage.getItem("save"))
 }
