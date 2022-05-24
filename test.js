becf0425ad1a910019f4f77d1cbb2f384055ad70
MCi-007 <82216395+MCi-007@users.noreply.github.com>
2022-05-24 19:12:05 +0700

    aaaaa

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var sus = 105
 var amogus = 0;
 window.setInterval(function() {
-    sus = Math.floor(sus + (0.5 * amogus))
+    sus = Math.floor(sus + (1 * amogus))
 }, 90);
 window.setInterval(function() {
     document.getElementById("sus").innerHTML = sus
@@ -10,7 +10,7 @@ window.setInterval(function() {
 },50);
 function Amogus() {  
     amogus = amogus + 1
-    sus = sus - (100+(amogus * 5))
+    sus = sus - (100 + (amogus * 5))
 ;}
 function Save() {
     var save = {
