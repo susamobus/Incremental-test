482bb48d4e3e36e081bf0bb19078f4060d6faff1
MCi-007 <82216395+MCi-007@users.noreply.github.com>
2022-06-08 21:40:24 +0700

    part of convertsuffix.js done

---
+++ suffixconvert.js
@@ -0,0 +1,41 @@
+function toSuffixes(num){
+  if (ExpantaNum.gte(num,ExpantaNum.pow(10,6)) && ExpantaNum.lt(num,ExpantaNum.pow(10,9)) == true) {
+      return [(ExpantaNum.round(ExpantaNum.div(num,10000))),M].join("")
+  }else if (ExpantaNum.gte(num,ExpantaNum.pow(10,9)) && ExpantaNum.lt(num,ExpantaNum.pow(10,12)) == true) {
+      
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+ 
